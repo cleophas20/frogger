b0560400a907ecb7719f72d126525e22395bce0b
Cleophas <cleophasfoy@gmail.com>
2022-09-08 10:59:00 -0500

    added app.js and movement codes

---
+++ app.js
@@ -0,0 +1,28 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+console.log(squares)
+let currentIndex = 76
+
+
+function moveFrog(e) {
+  switch(e.key) {
+    case 'ArrowLeft' :
+      console.log('move left')
+      break
+    case 'ArrowRight' :
+      console.log('move right')
+      break
+    case 'ArrowDown' :
+      console.log('move down')
+      break
+    case 'ArrowUp' :
+      console.log('move up')
+      break
+  }
+  
+  squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup',moveFrog)
